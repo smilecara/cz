38e94441087846a539b4140d67a73b9feec78113
cara.zhang <cara.zhang@perficient.com>
2020-09-01 15:42:45 +0800

    init cz project

---
+++ src/main/java/com/star/cz/CzApplication.java
@@ -0,0 +1,13 @@
+package com.star.cz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzApplication.class, args);
+	}
+
+}
